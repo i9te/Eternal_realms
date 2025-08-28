5823b70da900a556454b62b01a6c29457caf1649
i9te <rikeaji28@gmail.com>
2025-08-28 18:35:11 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
+  server: {
+    host: "0.0.0.0",
+    port: 5173
+  }
 });
