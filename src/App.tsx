0060beb1e77fb4c5d08b65c1eb099973ac9c0159
i9te <rikeaji28@gmail.com>
2025-08-28 17:40:35 +0700

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import CharacterSelect from "./pages/CharacterSelect";
+import Game from "./pages/Game";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/characters" element={<CharacterSelect />} />
+        <Route path="/game" element={<Game />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
